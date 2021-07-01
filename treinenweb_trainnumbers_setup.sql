8ae3519238f82ad251ce2924941fbed6fbb047b1
Ariën Claij <clay.aar@gmail.com>
2021-07-01 11:45:42 +0200

    Update treinenweb_trainnumbers_setup.sql
    Some fixes in the structure....

--- treinenweb_trainnumbers_setup.sql
+++ treinenweb_trainnumbers_setup.sql
@@ -1,45 +1,24 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server versie:                10.1.38-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win32
--- HeidiSQL Versie:              11.2.0.6213
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE IF NOT EXISTS `trainsdatabase`;
+USE `trainsdatabase`;
 
-
--- Databasestructuur van treinenweb_trains wordt geschreven
-CREATE DATABASE IF NOT EXISTS `treinenweb_trains` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `treinenweb_trains`;
-
--- Structuur van  tabel treinenweb_trains.trains_columns wordt geschreven
 CREATE TABLE IF NOT EXISTS `trains_columns` (
   `ID` int(4) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) NOT NULL DEFAULT '0',
-  `TrainID` tinyint(4) NOT NULL,
-  `Protected` tinyint(4) DEFAULT NULL,
-  `Sequence` tinyint(4) DEFAULT NULL,
-  `Description` tinyint(4) DEFAULT NULL,
+  `TrainID` int(4) NOT NULL,
+  `Protected` int(4) DEFAULT NULL,
+  `Sequence` int(4) DEFAULT NULL,
+  `Description` varchar(225) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Data exporteren was gedeselecteerd
 
--- Structuur van  tabel treinenweb_trains.trains_dates wordt geschreven
 CREATE TABLE IF NOT EXISTS `trains_dates` (
-  `NumberID` int(11) DEFAULT NULL,
+  `NumberID` int(4) DEFAULT NULL,
   `ColumnID` int(4) DEFAULT NULL,
   `Date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporteren was gedeselecteerd
-
--- Structuur van  tabel treinenweb_trains.trains_numbers wordt geschreven
 CREATE TABLE IF NOT EXISTS `trains_numbers` (
   `ID` int(4) NOT NULL AUTO_INCREMENT,
   `Number` int(4) DEFAULT NULL,
@@ -52,10 +31,3 @@ CREATE TABLE IF NOT EXISTS `trains_numbers` (
   `Description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Data exporteren was gedeselecteerd
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
